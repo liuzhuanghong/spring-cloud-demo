b8a62714af3f3178507287ab6aa547a3bd69e963
liuzhuanghong <liuzhuanghong@sina.com>
2017-02-01 21:36:47 +0800

    增加测试默认的负载均衡方法：轮巡

--- microservice-comsumer-student/src/main/java/com/steven/microservice/comsumer/controller/StudentComsumerController.java
+++ microservice-comsumer-student/src/main/java/com/steven/microservice/comsumer/controller/StudentComsumerController.java
@@ -3,6 +3,8 @@ package com.steven.microservice.comsumer.controller;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +19,26 @@ public class StudentComsumerController {
 	@Value("${student.studentServicePath}")
 	private String studentServicePath;
 	private final Logger logger = Logger.getLogger(getClass());
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
 
 	@GetMapping("/student/{id}")
 	public Student findById(@PathVariable Long id) {
+
 		logger.info("serivceURL: " + studentServicePath + id);
 		return this.restTemplate.getForObject(studentServicePath + id, Student.class);
+	}
+
+	/**
+	 * 测试默认的负载均衡方法：轮巡
+	 * 
+	 * @return 测试使用的字符串
+	 */
+	@GetMapping("/testLoadBalancer")
+	public String test() {
+		ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-provider-student");
+		logger.info("testLoadBalancerClient:[" + serviceInstance.getServiceId() + "/" + serviceInstance.getHost() + "/"
+				+ serviceInstance.getPort() + "]");
+		return "success";
 	}
 }
