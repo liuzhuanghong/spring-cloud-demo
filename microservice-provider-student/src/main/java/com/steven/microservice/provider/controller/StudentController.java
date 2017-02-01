c5f863ae7535ab58d23f444bb4a0453a4662ada4
liuzhuanghong <liuzhuanghong@sina.com>
2017-02-01 21:38:39 +0800

    增加服务调用时，记录服务提供方名称、端口等信息。

--- microservice-provider-student/src/main/java/com/steven/microservice/provider/controller/StudentController.java
+++ microservice-provider-student/src/main/java/com/steven/microservice/provider/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.steven.microservice.provider.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,11 @@ public class StudentController {
 	@Autowired
 	private StudentRepository studentRepository;
 
+	@Autowired
+	private DiscoveryClient client;
+
+	private final Logger logger = Logger.getLogger(getClass());
+
 	/**
 	 * 根据学生ID获取学生的学生类信息对象
 	 * 
@@ -24,6 +32,9 @@ public class StudentController {
 	// @GetMapping("/{id}")是spring 4.3的新注解等价于：
 	// @RequestMapping(value = "/id", method = RequestMethod.GET)
 	public Student findById(@PathVariable Long id) {
+		// 获取当前服务实例
+		ServiceInstance instance = client.getLocalServiceInstance();
+		logger.info("findById:[" + instance.getHost() + "/" + instance.getServiceId() + "/" + instance.getPort() + "]");
 		// replacing studentRepository.getOne(id) method call with
 		// studentRepository.findOne(id) problem was gone.
 		return studentRepository.findOne(id);
